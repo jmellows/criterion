765d4c9b3ae1ff9e3cfcce42e549074708e829bc
John Mellows <jmellowsjnr@gmail.com>
2014-04-15 12:57:10 +1200

    new structure implemented

--- client/js/script.js
+++ client/js/script.js
@@ -3,4 +3,18 @@ Template.index.rendered = function () {
 		anchors: ['firstPage', 'secondPage'],
 		menu: '#menu'
 	});
+
+	$( "#menu1" ).click(function() {
+		$.fn.fullpage.moveTo("secondPage");
+		$('#mainnav1').hide();
+		$('#mainnav2').fadeIn(1000);
+		/*$("#mainnav").removeClass("navbar-fixed-bottom").addClass( "navbar-fixed-top" );*/
+	});
+
+	$( "#menu2" ).click(function() {
+		$.fn.fullpage.moveTo("firstPage");
+		$('#mainnav1').fadeIn(1000);
+		$('#mainnav2').hide();
+		/*$("#mainnav").removeClass("navbar-fixed-bottom").addClass( "navbar-fixed-top" );*/
+	});
 };
